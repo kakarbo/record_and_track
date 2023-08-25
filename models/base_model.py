7d0fc125187cbf78631ba32c480f5bac0d8e51aa
kakarbo <alexcarbo8809@gmail.com>
2023-08-25 17:06:24 -0500

    instanciando el objeto con sus atributos correspondiente

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,11 @@ class BaseModel:
     id = Column(String(60), primary_key=True)
     created_at = Column(DateTime, default=datetime.utcnow)
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
 
