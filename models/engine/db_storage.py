6880a88831aabfd61a08913bd874179e89da4e54
kakarbo <alexcarbo8809@gmail.com>
2023-08-26 11:05:48 -0500

    actualizando metodos

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -37,12 +37,20 @@ class DBStorage:
     def all(self, cls=None, attribute=None, value=False):
         '''query on the current database session'''
         new_dict = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).filter(attribute <= value).all()
-                for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    new_dict[key] = obj
+        if value == True or value == False:
+            for clss in classes:
+                if cls is None or cls is classes[clss] or cls is clss:
+                    objs = self.__session.query(classes[clss]).filter(attribute == value).all()
+                    for obj in objs:
+                        key = obj.__class__.__name__ + '.' + obj.id
+                        new_dict[key] = obj
+        else:
+            for clss in classes:
+                if cls is None or cls is classes[clss] or cls is clss:
+                    objs = self.__session.query(classes[clss]).filter(attribute <= value).all()
+                    for obj in objs:
+                        key = obj.__class__.__name__ + '.' + obj.id
+                        new_dict[key] = obj
         return new_dict
 
     def new(self, obj):
